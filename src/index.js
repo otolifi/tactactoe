5be50234659e61e4d0e442ff3684b2200158fe83
Otoniel <otolifi@gmail.com>
2020-03-29 21:13:56 -0300

    changes1

---
+++ src/index.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+class Square2 extends React.Component {
+  render() {
+    return (
+      <div>
+        <button className="square" onClick={() => this.props.onClick()}>
+        {this.props.value}
+        </button>
+    </div>)
+  }
+}
+
+function Square(props) {
+  return (
+    <button className="square" onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+class Board extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true
+    }
+  }
+  renderSquare(i) {
+    return <Square value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}/>
+  }
+  handleClick(i) {
+    const squares = this.state.squares.slice()
+    squares[i] = this.state.xIsNext ? 'X' : 'O'
+    this.setState({squares: squares,
+    xIsNext: !this.state.xIsNext})
+  }
+  render() {
+    const status = "Next Player: " + (this.state.xIsNext ? 'X' : 'O')
+    return (
+      <div>
+        <div className="status">{status}</div>
+        <div className="board-row">
+          {this.renderSquare(0)}
+          {this.renderSquare(1)}
+          {this.renderSquare(2)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(3)}
+          {this.renderSquare(4)}
+          {this.renderSquare(5)}
+        </div>
+        <div className="board-row">
+          {this.renderSquare(6)}
+          {this.renderSquare(7)}
+          {this.renderSquare(8)}
+        </div>
+      </div>
+    )
+  }
+}
+
+class Game extends React.Component {
+  render() {
+    return (
+      <div className="game">
+        <div className="game-board">
+          <Board/>
+        </div>
+        <div className="game-info">
+          <div>{/* status */}</div>
+          <ol>{/* to do */}</ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Game />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
